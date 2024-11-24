ec4f26258d0b960e25c81b2cacd2b4c121805e0e
JahidHasan010 <jahidjahid36452@gmail.com>
2024-11-24 23:06:03 +0800

    mongodb notebook ,logger,exception and utils added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flight_price",
+    version="0.0.1",
+    author="Jahid Hasan",
+    author_email="jahidjahid36452@gmail.com",
+    packages=find_packages()
+)
